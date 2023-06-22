3389d828171101b38a13c7546fc058288e6f7b3b
choiruladamm <choiruladamm@gmail.com>
2023-06-22 12:53:42 +0700

    Reset Code

--- shopping-list/src/App.jsx
+++ shopping-list/src/App.jsx
@@ -1,49 +1,8 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable react/prop-types */
 
-import { useState } from "react";
-
-export default function App() {
-  const [items, setItems] = useState([]);
-
-  const onSubmit = (event) => {
-    event.preventDefault();
-    const form = event.target;
-    const input = form.item;
-    const newItems = [...items, input.value];
-    setItems(newItems);
-    form.reset();
-  };
-
-  const onRemoveItem = () => {};
-
+const App = () => {
   return (
-    <>
-      <h1>Shopping List</h1>
-      <div className="shopping-list">
-        <h2>Items to Buy</h2>
-        <form onSubmit={onSubmit}>
-          <input
-            type="text"
-            name="item"
-            placeholder="Add a new item"
-            required
-          />
-          <button className="button">Add</button>
-        </form>
-        {items.map((item, index) => (
-          <Item onRemoveItem={onRemoveItem} key={item + index} item={item} />
-        ))}0
-      </div>
-    </>
-  );
+    <div>App</div>
+  )
 }
 
-export const Item = ({ item, onRemoveItem }) => {
-  return (
-    <li>
-      {item}
-      <button onClick={onRemoveItem}>Remove</button>
-    </li>
-  );
-};
+export default App
