c21110d4cd7abf5017cd777b25c36fd1a7f43b61
choiruladamm <choiruladamm@gmail.com>
2023-06-27 08:32:29 +0700

    Github User Search

---
+++ github-search-user/src/App.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className=''>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos nesciunt vel facilis unde quibusdam quidem a facere, quisquam eos ducimus recusandae. Sunt voluptatibus, quo itaque atque nemo fugit consectetur quaerat similique harum necessitatibus hic. Quibusdam ea doloribus magni dignissimos tempore? Excepturi nulla necessitatibus sunt a, asperiores omnis non laudantium cumque voluptatum quos. Saepe, dolor voluptatem atque minus ducimus porro repellendus earum autem laudantium, nobis, necessitatibus praesentium suscipit itaque numquam alias! Eum, impedit dicta, maxime atque excepturi praesentium a distinctio explicabo quae eius odit dolorum voluptas. Consectetur non animi, minus tenetur eius perferendis facilis modi fugit earum nulla iusto! Rem, assumenda.
+    </div>
+  )
+}
+
+export default App
