67e3adb4ab510f23b1e901a8d2dc732ca6e3511e
choiruladamm <choiruladamm@gmail.com>
2023-06-29 19:20:12 +0700

    Completed BMI Calculator

--- bmi-calculator/src/App.jsx
+++ bmi-calculator/src/App.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 
-import { useState } from "react";
+import { useMemo, useState } from "react";
 
 const DEFAULT_WEIGHT = 50;
 const DEFAULT_HEIGHT = 150;
@@ -15,16 +15,22 @@ const App = () => {
     console.log(inputWeight);
   };
 
+  const output = useMemo(() => {
+    const calculatedHeight = height / 100
+    return (weight / (calculatedHeight * calculatedHeight)).toFixed(1)
+  }, [weight, height])
+
   return (
     <div className="h-screen grid place-items-center">
       <div className="card max-w-lg bg-gray-50 border border-gray-300 rounded-lg">
-        <div className="heading h-20 bg-slate-900 rounded-t-lg grid place-items-center">
+        
+        <div className="heading py-5 bg-slate-900 rounded-t-lg grid place-items-center">
           <p className="text-white font-extrabold text-2xl tracking-tight">
             BMI CALCULATOR
           </p>
         </div>
 
-        <div className="content p-10 w-[25rem]">
+        <div className="content px-10 py-7 w-[25rem]">
           {/* Weight Slider */}
           <label
             htmlFor="minmax-range"
@@ -54,6 +60,15 @@ const App = () => {
             max="220"
             className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer "
           />
+
+          <div className="container-output text-base font-medium text-gray-900 mt-5 text-center">
+            Your BMI Is
+            <br />
+            <div className="output bg-slate-900 inline-block text-white font-bold px-6 py-2 rounded-md mt-2">
+              {output}
+            </div>
+          </div>
+
         </div>
       </div>
     </div>
