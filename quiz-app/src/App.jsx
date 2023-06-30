593e030671fe07d8a5a5e30e1149458768695707
choiruladamm <choiruladamm@gmail.com>
2023-06-30 16:13:23 +0700

    Styling Welcome Page

--- quiz-app/src/App.jsx
+++ quiz-app/src/App.jsx
@@ -4,7 +4,12 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 const App = () => {
+  // all quiz
   const [quizs, setQuizs] = useState([]);
+
+  // displaying controller
+  const [showStart, setShowStart] = useState(true);
+  const [showQuiz, setShowQuiz] = useState(false);
 
   // load data json
   useEffect(() => {
@@ -16,9 +21,35 @@ const App = () => {
       });
   }, []);
 
+  // start quiz
+  const startQuiz = () => {
+    setShowStart(false);
+    setShowQuiz(true);
+  };
+
   return (
     <>
-      <h1>react bootstrap</h1>
+      {/* welcome page */}
+      <section
+        className="text-white text-center bg-dark"
+        style={{ display: `${showStart ? "block" : "none"}` }}
+      >
+        <div className="container">
+          <div className="row vh-100 align-items-center justify-content-center">
+            <div className="col-lg-8">
+              <h1 className="fw-bold mb-4">Basic ReactJS Quiz</h1>
+              <button
+                onClick={startQuiz}
+                className="btn bg-light px-4 py-2 text-dark fw-bold"
+              >
+                Start Quiz
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* quiz page */}
     </>
   );
 };
