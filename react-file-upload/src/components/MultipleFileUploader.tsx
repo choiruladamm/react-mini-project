672fb39593b1d6d1ed24adaad522483c9c86d677
choiruladamm <choiruladamm@gmail.com>
2023-07-22 13:54:22 +0700

    Finished Multiple File Upload

--- react-file-upload/src/components/MultipleFileUploader.tsx
+++ react-file-upload/src/components/MultipleFileUploader.tsx
@@ -1,5 +1,91 @@
+import { useState } from "react";
+
 const MultipleFileUploader = () => {
-  return <div>MultipleFileUploader</div>;
+  const [files, setFiles] = useState<FileList | null>(null);
+  const [status, setStatus] = useState<
+    "initial" | "uploading" | "success" | "fail"
+  >("initial");
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      setStatus("initial");
+      setFiles(e.target.files);
+    }
+  };
+
+  const handleUpload = async () => {
+    if (files) {
+      setStatus("uploading");
+
+      const formData = new FormData();
+
+      [...files].forEach((file) => {
+        formData.append("files", file);
+      });
+
+      try {
+        const result = await fetch("https://httpbin.org/post", {
+          method: "POST",
+          body: formData,
+        });
+
+        const data = await result.json();
+        console.log(data);
+        setStatus("success");
+      } catch (error) {
+        console.error(error);
+        setStatus("fail");
+      }
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <label htmlFor="file" className="sr-only">
+          Choose a file
+        </label>
+        <input type="file" id="file" multiple onChange={handleFileChange} />
+      </div>
+
+      {files &&
+        [...files].map((file, index) => (
+          <section
+            style={{
+              marginTop: "20px",
+            }}
+            key={file.name}
+          >
+            File number {index + 1} details:
+            <ul>
+              <li>Name: {file.name}</li>
+              <li>Type: {file.type}</li>
+              <li>Size: {file.size} bytes</li>
+            </ul>
+          </section>
+        ))}
+
+      {files && (
+        <button type="submit" onClick={handleUpload}>
+          Upload {files.length > 1 ? "files" : "a file"}
+        </button>
+      )}
+
+      <Result status={status} />
+    </>
+  );
+};
+
+const Result = ({ status }: { status: string }) => {
+  if (status === "success") {
+    return <p>✅ File uploaded successfully!</p>;
+  } else if (status === "fail") {
+    return <p>❌ File upload failed!</p>;
+  } else if (status === "uploading") {
+    return <p>⏳ Uploading selected file...</p>;
+  } else {
+    return null;
+  }
 };
 
 export default MultipleFileUploader;
