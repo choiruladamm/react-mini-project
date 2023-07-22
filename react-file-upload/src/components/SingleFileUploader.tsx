4dd4b5d21db8d2f2ae2d8b79dfafea2c853cfa04
choiruladamm <choiruladamm@gmail.com>
2023-07-22 12:48:39 +0700

    Add Logic File Upload

--- react-file-upload/src/components/SingleFileUploader.tsx
+++ react-file-upload/src/components/SingleFileUploader.tsx
@@ -2,16 +2,38 @@ import { useState } from "react";
 
 const SingleFileUploader = () => {
   const [file, setFile] = useState<File | null>(null);
+  const [status, setStatus] = useState<
+    "initial" | "uploading" | "success" | "fail"
+  >("initial");
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files) {
+      setStatus("initial");
       setFile(e.target.files[0]);
     }
   };
 
-  const handleUpload = () => {
-    console.log('files uploaded succesfully');
-    
+  const handleUpload = async () => {
+    if (file) {
+      setStatus("uploading");
+
+      const formData = new FormData();
+      formData.append("file", file);
+
+      try {
+        const result = await fetch("https://httpbin.org/post", {
+          method: "POST",
+          body: formData,
+        });
+
+        const data = await result.json();
+        console.log(data);
+        setStatus("success");
+      } catch (error) {
+        console.error(error);
+        setStatus("fail");
+      }
+    }
   };
 
   return (
@@ -34,9 +56,27 @@ const SingleFileUploader = () => {
         </section>
       )}
 
-      {file && <button onClick={handleUpload}>Upload a file</button>}
+      {file && (
+        <button type="submit" onClick={handleUpload}>
+          Upload a file
+        </button>
+      )}
+
+      <Result status={status} />
     </>
   );
+};
+
+const Result = ({ status }: { status: string }) => {
+  if (status === "success") {
+    return <p>✅ File uploaded successfully!</p>;
+  } else if (status === "fail") {
+    return <p>❌ File upload failed!</p>;
+  } else if (status === "uploading") {
+    return <p>⏳ Uploading selected file...</p>;
+  } else {
+    return null;
+  }
 };
 
 export default SingleFileUploader;
