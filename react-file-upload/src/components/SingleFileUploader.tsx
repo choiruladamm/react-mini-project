faaf2ee2a518c525d1b6ff68deed3b6112280e06
choiruladamm <choiruladamm@gmail.com>
2023-07-22 12:32:03 +0700

    Add Single File Components & Styling

--- react-file-upload/src/components/SingleFileUploader.tsx
+++ react-file-upload/src/components/SingleFileUploader.tsx
@@ -1,41 +1,42 @@
-import{ useState } from 'react'
+import { useState } from "react";
 
 const SingleFileUploader = () => {
-  const [file, setFile] = useState<File | null>(null)
+  const [file, setFile] = useState<File | null>(null);
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files) {
-      setFile(e.target.files[0])  
+      setFile(e.target.files[0]);
     }
-  }
+  };
 
   const handleUpload = () => {
-
-  }
+    console.log('files uploaded succesfully');
+    
+  };
 
   return (
     <>
       <div>
-        <label htmlFor='file' className="sr-only">
+        <label htmlFor="file" className="sr-only">
           Choose a file
         </label>
         <input type="file" id="file" onChange={handleFileChange} />
       </div>
 
       {file && (
-        <section>
+        <section style={{ marginTop: "20px" }}>
           File details:
           <ul>
             <li>Name: {file.name}</li>
-            <li>Name: {file.type}</li>
-            <li>Name: {file.size} bytes</li>
+            <li>Type: {file.type}</li>
+            <li>Size: {file.size} bytes</li>
           </ul>
         </section>
       )}
 
-      {file && <button onChange={handleUpload}>Upload a file</button>}
+      {file && <button onClick={handleUpload}>Upload a file</button>}
     </>
-  )
-}
+  );
+};
 
-export default SingleFileUploader
+export default SingleFileUploader;
